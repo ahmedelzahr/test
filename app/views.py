31345105c54a0deb342ac7127b62dc5f735dd1ce
ahmed elzahar <ah_elzahar@yahoo.com>
2018-03-07 18:06:08 +0300

    v.1.0.1

--- app/views.py
+++ app/views.py
@@ -5,6 +5,7 @@ from app import app, member_store, post_store
 @app.route("/")
 def home():
     return render_template("index.html",posts=post_store.get_all())
+
 @app.route("/topic/add", methods = ["GET", "POST"])
 def topic_add():
     if request.method == "POST":
@@ -14,3 +15,30 @@ def topic_add():
 
     else:
         return render_template("topic_add.html")
+
+@app.route("/tobic/delete/<id>")
+def topic_delete(id):
+    post_store.delete(int(id))
+    print id
+    return redirect(url_for("home"))
+
+@app.route("/tobic/edit/<id>", methods = ["GET", "POST"])  
+def topic_edit(id):
+    if request.method == "POST":
+    	post=post_store.get_by_id(int(id))
+        post.title = request.form["title"]
+        post.subject=request.form["content"]
+        post_store.update(post)
+        return redirect(url_for("home"))
+
+    else:
+        return render_template("topic_edit.html",po=post_store.get_by_id(int(id)))
+
+@app.route("/tobic/show/<id>", methods = ["GET", "POST"])  
+def topic_show(id):
+    if request.method == "POST":
+    	print "back"
+        return redirect(url_for("home"))
+
+    else:
+        return render_template("topic_show.html",po=post_store.get_by_id(int(id)))        
